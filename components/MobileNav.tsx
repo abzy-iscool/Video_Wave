71c7e9fad52b110e3b03083a1041f135e2e99289
Abhishek J <abhishekjani960@gmail.com>
2024-07-22 15:22:42 +0300

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -31,7 +31,7 @@ const MobileNav = () => {
               height={32}
               alt="video wave logo"
             />
-            <p className="text-[26px] font-extrabold text-white">Video Wave</p>
+            <p className="text-[26px] font-extrabold text-white">Synapse</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
