ac53f8c2d32fd13c9a54e352546a271f8557500d
Abhishek J <abhishekjani960@gmail.com>
2024-08-07 10:40:00 +0300

    Update Sidebar.tsx
    added "glassmorphism"

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
               href={item.route}
               key={item.label}
               className={cn(
-                'flex gap-4 items-center p-4 rounded-lg justify-start',
+                'glassmorphism flex gap-4 items-center p-4 rounded-lg justify-start',
                 {
                   'bg-blue-1': isActive,
                 }
